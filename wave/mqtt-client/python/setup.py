8d111ecebb81dfe6d022e03f34ea32cb72552b0e
Joshua Adkuins <adkins@berkeley.edu>
2018-09-20 19:13:55 -0700

    move around wave directory structure so that it can be used as a package

---
+++ wave/mqtt-client/python/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+from os import path
+from io import open
+
+setup(
+    name='wavemqtt',  # Required
+    version='0.0.1',  # Required
+    description='A client for wave-mqtt integration',  # Optional
+    url='https://github.com/conix-center/smart-cities-demo/tree/master/wave/mqtt-client/python/wavemqtt',  # Optional
+    author='Gabe Fierro',  # Optional
+    author_email='gtfierro@cs.berkeley.edu',  # Optional
+    keywords='wave mqtt access control entity',  # Optional
+    packages=find_packages(exclude=['examples', 'docs', 'tests']),  # Required
+    install_requires=['paho',
+                      'wave3',
+                      'pickle',
+                      'base64',
+                      'grpc',
+                      'threading',
+                      'json'],  # Optional
+    
+    dependency_links=['https://github.com/immesys/pywave'],
+)
