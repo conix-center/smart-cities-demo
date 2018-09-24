e92a087f9c47b0ec32718f569f04a9bae7834690
Joshua Adkuins <adkins@berkeley.edu>
2018-09-24 12:02:59 -0400

    bump to 0.0.7

--- client-library/poster/python/setup.py
+++ client-library/poster/python/setup.py
@@ -4,7 +4,7 @@ from io import open
 
 setup(
     name='conixposter',  # Required
-    version='0.0.6',  # Required
+    version='0.0.7',  # Required
     description='Interface to a conix administrative domain',  # Optional
     url='https://github.com/conix-center/smart-cities-demo/tree/master/client-library/poster/python/conixposter',  # Optional
     author='Joshua Adkins',  # Optional
