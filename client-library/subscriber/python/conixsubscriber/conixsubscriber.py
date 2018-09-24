5716294edb083386bd47684c706c921b7ae0ef1c
Joshua Adkuins <adkins@berkeley.edu>
2018-09-23 21:54:51 -0400

    this seems to be working other than the wave updating values part

--- client-library/subscriber/python/conixsubscriber/conixsubscriber.py
+++ client-library/subscriber/python/conixsubscriber/conixsubscriber.py
@@ -4,16 +4,18 @@ import time
 from aenum import Enum, auto
 import pint
 import psycopg2
+from psycopg2 import sql
+import operator
 
 class ConixSubscriber:
 
-    def __init__(self, client_id, 
-                domain_url = 'stream.conixdb.io', domain_username='conix', domain_password='stream', domain_port='8883', 
+    def __init__(self, client_id,
+                domain_url = 'stream.conixdb.io', domain_username='conix', domain_password='stream', domain_port='8883',
                 wave_uri='localhost:410',
                 database_username='username',
                 database_port='port',
                 database_password='password',
-                database_hoster='host'
+                database_host='host',
                 database_name='name'):
 
         #start up a conix client
@@ -23,39 +25,45 @@ class ConixSubscriber:
             mosquitto_user=domain_username,
             mosquitto_port=domain_port,
             mosquitto_tls = True,
-            wave_uri=wave_uri)
+            wave_uri=wave_uri,
+            on_message=waveCallback)
+
+        self.namespace = self.client.register(client_id)
 
         #initialize a psycopg2 instance
-        self.connection = psycopg2.connect(dbname=database_name, 
-                                            host=database_host, 
-                                            port=database_port, 
-                                            user=database_username, 
+        self.connection = psycopg2.connect(dbname=database_name,
+                                            host=database_host,
+                                            port=database_port,
+                                            user=database_username,
                                             password=database_password)
     """
     Current only takes single equals to condition
     """
     def subscribe(self, channels, condition, callback):
         self.callback = callback
-        
+
         # use psycopg2 to get a list of tables in the database
         cursor = self.connection.cursor()
         cursor.execute("""SELECT table_name FROM information_schema.tables
         WHERE table_schema = 'public'""")
-        tables = cursor.fetchall()
+        tables = [x[0] for x in cursor.fetchall()]
         tableDict = {}
+
 
         #remove uuid and timestamp from the list because those are standard
         channel_lower = [x.lower() for x in channels]
-        channel_lower.remove('uuid')
-        channel_lower.remove('timestamp')
+        if 'uuid' in channel_lower:
+            channel_lower.remove('uuid')
+        if 'timestamp' in channel_lower:
+            channel_lower.remove('timestamp')
 
         # now query each one of those tables to see if the table contains a
         # channel that we care about
         tableList = []
         for table in tables:
-            tableDict[table] = [] 
-            cursor.execute("select column_name from information_schema.columns where table_schema='public' AND table_name=%s",table)
-            columns = cursor.fetchall()
+            tableDict[table] = []
+            cursor.execute("""select column_name from information_schema.columns where table_schema='public' AND table_name=%s""",(table,))
+            columns = [x[0] for x in cursor.fetchall()]
             tableDict[table] = columns
             channel_set = set(channel_lower)
             column_set = set(columns)
@@ -74,13 +82,29 @@ class ConixSubscriber:
             else:
                 tableUUID[uuid] = []
                 tableUUID[uuid].append(table)
-        
+
         finalTableList = []
         if condition != '':
             #get column name in condition
-            condition_channel = condition.split('=')[0]
-            condition_value = condition.split('=')[1]
-            
+            later_op = None
+            split_char = None;
+            if condition.find('=') > -1:
+                split_char = '=';
+                later_op = operator.eq > -1;
+            elif condition.find('>'):
+                split_char = '>';
+                later_op = operator.gt;
+            elif condition.find('<') > -1:
+                split_char = '<';
+                later_op = operator.lt;
+            else:
+                raise ValueError("condition must contain =,>,<")
+
+            condition = condition.lower()
+            condition = "".join(condition.split())
+            condition_channel = condition.split(split_char)[0]
+            condition_value = condition.split(split_char)[1]
+
             #look for all tables with the condition we care about
             tablesToCheck = []
             for key in tableDict:
@@ -91,33 +115,67 @@ class ConixSubscriber:
             #condition, if does add it to a good uuid list
             goodUUID = []
             for table in tablesToCheck:
-                cursor.execute(sql.SQL("select %s from {} ORDER BY timestamp DESC limit 1").format(sql.Identifier(table)), condition_channel)
-                result = cursor.fetch()
-                if condition_value == result:
-                    #great this is a good uuid
-                    goodUUID.append(s.join(table.split('-')[:-1]))
-            
+                cursor.execute(sql.SQL("""select {} from {} ORDER BY timestamp DESC limit 1""").format(sql.Identifier(condition_channel), sql.Identifier(table)))
+                result = [x[0] for x in cursor.fetchall()][0]
+                if isinstance(result, str):
+                    if later_op(result, condition_value):
+                        #great this is a good uuid
+                        goodUUID.append(s.join(table.split('-')[:-1]))
+                elif isinstance(result, float):
+                    try:
+                        if later_op(result, float(condition_value)):
+                            #great this is a good uuid
+                            goodUUID.append(s.join(table.split('-')[:-1]))
+                    except:
+                        print('Error retyping condition')
+                elif isinstance(result, int):
+                    try:
+                        if later_op(result, int(condition_value)):
+                            #great this is a good uuid
+                            goodUUID.append(s.join(table.split('-')[:-1]))
+                    except:
+                        print('Error retyping condition')
+
             #intersect the uuids and good UUID lists to produce a final list
             uuid_set = set(uuids)
             good_uuid_set = set(goodUUID)
             final_set = uuid_set.intersection(good_uuid_set)
             for uuid in final_set:
-                finalTableList  = finalTableList + tableUUID[uuid] 
+                finalTableList  = finalTableList + tableUUID[uuid]
 
         else:
             finalTableList = tableList
-        
-        print(finalTableList)
 
         # query all the tables in the final table list
         # make a dict with uuid as key and the info the user cares about
-        # as the values 
+        # as the values
+        self.subscribeData = {}
+        for table in finalTableList:
+            cursor.execute(sql.SQL("""select * from {} ORDER BY timestamp DESC limit 1""").format(sql.Identifier(table)))
+            result = cursor.fetchall()[0]
+            columns = [desc[0] for desc in cursor.description]
+            if result[columns.index('uuid')] not in self.subscribeData:
+                self.subscribeData[result[columns.index('uuid')]] = {}
+
+            self.subscribeData[result[columns.index('uuid')]]['uuid'] = result[columns.index('uuid')]
+            self.subscribeData[result[columns.index('uuid')]]['timestamp'] = result[columns.index('timestamp')].timestamp()*1000000
+            for column in columns:
+                if column in channel_lower:
+                    self.subscribeData[result[columns.index('uuid')]][column] = result[columns.index(column)]
+
+        #make the initial callbacks to the user with the starting data
+        for key in self.subscribeData:
+            self.callback(self.subscribeData[key])
 
         #now turn this list of final tables into a list of MQTT topics
         #and subscribe through wave.
+        s = '-'
+        for table in finalTableList:
+            topic = s.join(table.split('-')[:-1])+'/'+ table.split('-')[-1]
+            self.client.subscribe(self.namespace, topic)
 
 def waveCallback(client, userdata, msg):
     # on callback go into the dict, update the information that was sent
     # in this message, then callback to the user with the updated dict
     # for that uuid
-
+    pass
--- client-library/subscriber/python/examples/subscribe.py
+++ client-library/subscriber/python/examples/subscribe.py
@@ -1,20 +1,27 @@
 #!/usr/bin/env python3
 
+import os, sys
 sys.path.append(os.path.join(os.path.dirname(__file__), '..', 'conixsubscriber'))
 import conixsubscriber
+import configparser
 
 #import the postgres config file
+postgresParser = configparser.ConfigParser()
 postgresParser.read('postgres.ini')
 postgresConf = postgresParser['DEFAULT']
 
-subscriber = conixsbuscriber.ConixSubscriber("subscribe", 
+subscriber = conixsubscriber.ConixSubscriber("subscribe",
                                             database_username = postgresConf['username'],
                                             database_port = postgresConf['port'],
                                             database_name = postgresConf['database'],
                                             database_password = postgresConf['password'],
                                             database_host = postgresConf['host'])
 
-def callback():
-    pass
+def callback(data):
+    print(data)
 
-subscriber.subscribe(['Temperature'],'uuid=8608a83a-b7a2-11e8-8755-0cc47a0f7777', callback)
+subscriber.subscribe(['Temperature','temperature_units'],'cpu_usage > 49.9', callback)
+
+import time
+while True:
+    time.sleep(1)
